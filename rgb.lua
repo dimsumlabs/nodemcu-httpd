8ebce7a70de0620ba4af22a4488439d36670a9e7
Lionello Lunesu <lio+git@lunesu.com>
2016-01-09 02:19:58 +0800

    Removed semi-colons and fixed indentation

--- rgb.lua
+++ rgb.lua
@@ -1,20 +1,22 @@
 return function (connection, req)
-   local arg = tonumber(req:match("r=([0-9]+)"));
-   if arg then
-     pwm.setduty(5, arg);
-   end
+  local setduty = pwm.setduty
 
-   arg = tonumber(req:match("g=([0-9]+)"));
-   if arg then
-     pwm.setduty(6, arg);
-   end
+  local arg = tonumber(req:match("r=([0-9]+)"))
+  if arg then
+    setduty(5, arg)
+  end
 
-   arg = tonumber(req:match("b=([0-9]+)"));
-   if arg then
-     pwm.setduty(7, arg);
-   end
+  arg = tonumber(req:match("g=([0-9]+)"))
+  if arg then
+    setduty(6, arg)
+  end
 
-   -- Send back JSON response.
-   connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\nConnection: close\r\n\r\n{'error':0, 'message':'OK'}")
-   connection:close()
+  arg = tonumber(req:match("b=([0-9]+)"))
+  if arg then
+    setduty(7, arg)
+  end
+
+  -- Send back JSON response.
+  connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\nConnection: close\r\n\r\n{'error':0, 'message':'OK'}")
+  connection:close()
 end
