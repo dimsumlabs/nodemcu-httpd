a0ce06e1111182c38928d7615d0bd63f888fbec5
Lionello Lunesu <lio+git@lunesu.com>
2016-01-09 18:12:14 +0800

    Added checkbox to index.html for toggle adc

--- adc.lua
+++ adc.lua
@@ -1,43 +1,43 @@
 return function (connection, req)
   local ms = tonumber(req:match("ms=([0-9]+)"))
-  if ms then
-    connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\nConnection: close\r\n\r\n")
-    connection:close()
+  if not ms then
+    if lastms then ms=5 lastms=nil else ms=0 lastms=true end
+  end
 
-    tmr.stop(1)
-    if ms > 0 then
-      tmr.stop(0)
-      saveColor()
-      local last = 0;
-      local setduty = pwm.setduty
-      local function sample()
-        local delta = 1024 - adc.read(0)
-        if delta ~= last then
-          last = delta
-          local a = R + delta
-          if a > 1023 then a = 1023 end
-          setduty(5, a)
-          a = G + delta
-          if a > 1023 then a = 1023 end
-          setduty(6, a)
-          a = B + delta
-          if a > 1023 then a = 1023 end
-          setduty(7, a)
-        end
-        --connection:send(delta..",\n")
+  connection:send("HTTP/1.0 302 Found\r\nLocation: /\r\nCache-Control: private, no-store\r\nConnection: close\r\n\r\n"..ms)
+  connection:close()
+
+  tmr.stop(0)
+  tmr.stop(1)
+  if ms > 0 then
+    saveColor()
+    local lastdelta = 0;
+    local setduty = pwm.setduty
+
+    local function sample()
+      local delta = 1024 - adc.read(0)
+      if delta < lastdelta - 10 then delta = lastdelta - 10 end
+      if delta ~= lastdelta then
+        lastdelta = delta
+        local a = R + delta
+        if a > 1023 then a = 1023 end
+        setduty(5, a)
+        a = G + delta
+        if a > 1023 then a = 1023 end
+        setduty(6, a)
+        a = B + delta
+        if a > 1023 then a = 1023 end
+        setduty(7, a)
       end
-
-      tmr.alarm(1, ms, 1, sample)
-    else
-      -- Restore the old color
-      pwm.setduty(5, R)
-      pwm.setduty(6, G)
-      pwm.setduty(7, B)
-      tmr.alarm(0, 5000, 1, saveColor)
+      --connection:send(delta..",\n")
     end
+
+    tmr.alarm(1, ms, 1, sample)
   else
-    -- Send back JSON response.
-    connection:send("HTTP/1.0 400 Bad Request\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\nConnection: close\r\n\r\n{'missing':'ms'}")
-    connection:close()
+    -- Restore the old color
+    pwm.setduty(5, R)
+    pwm.setduty(6, G)
+    pwm.setduty(7, B)
+    tmr.alarm(0, 5000, 1, saveColor)
   end
 end
