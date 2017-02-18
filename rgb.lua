4705428597a254d2564d8dd969a8c41158194838
Steph Ng <recurza@gmail.com>
2017-02-18 21:47:03 +0800

    Add crossfade and update UI

--- rgb.lua
+++ rgb.lua
@@ -1,22 +1,51 @@
+PIN_R = 5
+PIN_G = 6
+PIN_B = 7
+
+FADE_MS = 100
+
 return function (connection, req)
+  local getduty = pwm.getduty
   local setduty = pwm.setduty
 
   local arg = tonumber(req:match("r=([0-9]+)"))
   if arg then
-    setduty(5, arg)
+    -- fade(getduty(PIN_R), arg, FADE_MS, function(val)
+      setduty(PIN_R, arg)
+    -- end)
   end
 
   arg = tonumber(req:match("g=([0-9]+)"))
   if arg then
-    setduty(6, arg)
+    -- fade(getduty(PIN_G), arg, FADE_MS, function(val)
+    var = arg
+      setduty(PIN_G, arg)
+    -- end)
   end
 
   arg = tonumber(req:match("b=([0-9]+)"))
   if arg then
-    setduty(7, arg)
+    -- fade(getduty(PIN_B), arg, FADE_MS, function(val)
+    var = arg
+      setduty(PIN_B, arg)
+    -- end)
   end
 
   -- Send back JSON response.
   connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\nConnection: close\r\n\r\n{'error':0, 'message':'OK'}")
   connection:close()
+end
+
+function fade(a, b, freq, callback)
+  local inc = 10
+  local timer = tmr.create()
+  timer:alarm(30, tmr.ALARM_AUTO, function(timer)
+    if a < b then
+      callback(a)
+      a = a + inc
+    else
+      callback(b)
+      timer:unregister()
+    end
+  end)
 end
