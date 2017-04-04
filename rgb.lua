301394da2a4ad103de1719ca55ea37eb7f34bf7c
Stephanie Ng <recurza@gmail.com>
2017-04-04 17:54:13 +0800

    Put js code into html and replace old rgb.lua with correct file

--- rgb.lua
+++ rgb.lua
@@ -2,50 +2,45 @@ PIN_R = 5
 PIN_G = 6
 PIN_B = 7
 
-FADE_MS = 100
+FADE_MS = 8
+INTERVAL = 5
+getduty = pwm.getduty
+setduty = pwm.setduty
+
+function fade(pin, x, tid)
+  local a = getduty(pin)
+  local mult = 1
+  if a>x then mult = -1 end
+
+  tmr.alarm(tid, FADE_MS, tmr.ALARM_AUTO, function()
+    if math.abs(x-a) <= INTERVAL then
+      setduty(pin, x)
+      tmr.unregister(tid)
+    elseif a~=x then
+      setduty(pin, a)
+      a = a + (INTERVAL * mult)
+    end
+  end)
+end
 
 return function (connection, req)
-  local getduty = pwm.getduty
-  local setduty = pwm.setduty
+  local r = tonumber(req:match("r=([0-9]+)"))
+  local g = tonumber(req:match("g=([0-9]+)"))
+  local b = tonumber(req:match("b=([0-9]+)"))
 
-  local arg = tonumber(req:match("r=([0-9]+)"))
-  if arg then
-    -- fade(getduty(PIN_R), arg, FADE_MS, function(val)
-      setduty(PIN_R, arg)
-    -- end)
+  if r then
+    fade(PIN_R, r, 0)
   end
 
-  arg = tonumber(req:match("g=([0-9]+)"))
-  if arg then
-    -- fade(getduty(PIN_G), arg, FADE_MS, function(val)
-    var = arg
-      setduty(PIN_G, arg)
-    -- end)
+  if g then
+    fade(PIN_G, g, 1)
   end
 
-  arg = tonumber(req:match("b=([0-9]+)"))
-  if arg then
-    -- fade(getduty(PIN_B), arg, FADE_MS, function(val)
-    var = arg
-      setduty(PIN_B, arg)
-    -- end)
+  if b then
+    fade(PIN_B, b, 2)
   end
 
   -- Send back JSON response.
   connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\nConnection: close\r\n\r\n{'error':0, 'message':'OK'}")
   connection:close()
-end
-
-function fade(a, b, freq, callback)
-  local inc = 10
-  local timer = tmr.create()
-  timer:alarm(30, tmr.ALARM_AUTO, function(timer)
-    if a < b then
-      callback(a)
-      a = a + inc
-    else
-      callback(b)
-      timer:unregister()
-    end
-  end)
 end
